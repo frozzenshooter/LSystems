1e23747b04e7c1d7acca261b7e4ad209de424f22
Steffen Knoblauch <knoblauch.steffen@web.de>
2020-07-02 12:41:33 +0200

    SImplified LSystem2D

---
+++ src/LSystem2D.cpp
@@ -0,0 +1,60 @@
+#include "LSystem2D.hpp"
+
+void LSystem2D::CalculateNextGeneration() {
+
+    std::string nextState;
+
+    for (int i = 0; i < currentState.size(); ++i) {
+
+        auto key = currentState[i];
+
+        auto production = production_rules[key];
+
+        // TODO: better way to append string/char ?
+        if (!production.empty()) {
+
+            nextState.append(production);
+
+        } else {
+            nextState.push_back(key);
+        }
+    }
+
+    currentState = nextState;
+}
+
+LSystem2D::LSystem2D(const std::string& startaxiom): startAxiom(startaxiom), currentState("") {}
+
+/*
+*   Adds a production rule consiting of a nonTerminal which will be replaced with the production
+*/
+void LSystem2D::add_Production_Rule(const char nonTerminal, const std::string& production) {
+        
+    // TODO: validate rule (is already the same rule there + do i override a rule)
+    production_rules.insert(std::pair(nonTerminal, production));
+}
+
+void LSystem2D::remove_Production_Rules() noexcept{
+    production_rules.clear();
+}
+
+/*
+* Calculates the resulting string of the given LSystem
+*
+* generation: how often the rules should be applied to the current state to get the result
+*
+*/
+const std::string& LSystem2D::get_result(const int generation) {
+       
+    currentState = startAxiom;
+
+    if (production_rules.size() > 0) {
+        
+        for (int i = 0; i < generation; ++i) {
+
+            CalculateNextGeneration();
+        }
+    }
+
+    return currentState;
+}
