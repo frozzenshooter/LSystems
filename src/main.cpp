1b484ba7c40ec3928cbe71fbc53ce2e4744d92de
Steffen Knoblauch <knoblauch.steffen@web.de>
2020-07-02 12:20:24 +0200

    Small improvments

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,24 @@
 
 int main() {
 
-    LSystem2D testLSystem{"b"};
+    LSystem2D testLSystem{ "b" };
 
     testLSystem.add_Production_Rule('a', "ab");
     testLSystem.add_Production_Rule('b', "a");
 
-    for (int i = 0; i < 20; ++i) {
+    for (int i = 0; i < 10; ++i) {
         std::cout << testLSystem.get_result(i) << std::endl;
     }
+
+    LSystem2D test2{ "F" };
+    test2.add_Production_Rule('F', "F+F--F+F");
+
+    for (int i = 0; i < 3; ++i) {
+        std::cout << test2.get_result(i) << std::endl;
+    }
+
+
+
     std::cin.get();
     return 0;
 }
