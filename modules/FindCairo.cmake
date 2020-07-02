75acd0215632ba2d54ec5f5012967eff9704d2ed
Steffen Knoblauch <knoblauch.steffen@web.de>
2020-07-02 18:12:43 +0200

    Cairo include fixed for windows (only x64 builds supported for now)

--- modules/FindCairo.cmake
+++ modules/FindCairo.cmake
@@ -1,21 +1,20 @@
 include(FindPackageHandleStandardArgs)
 
 if(WIN32)
+
     # Search for Cairo in extern/cairo-windows-1.15.12
     find_path(Cairo_ROOT "include/cairo.h" PATHS "${CMAKE_CURRENT_LIST_DIR}/../lib/cairo" NO_DEFAULT_PATH)
+    
+    # choose correct dlls for windows 64Bit
     if(Cairo_ROOT)
         set(Cairo_INCLUDE_DIRS "${Cairo_ROOT}/include")
-        if("${CMAKE_GENERATOR}" MATCHES "Win64")
-            set(Cairo_LIBS "${Cairo_ROOT}/lib/x64/cairo.lib")
-            set(Cairo_DLLS "${Cairo_ROOT}/lib/x64/cairo.dll")
-        else()
-            set(Cairo_LIBS "${Cairo_ROOT}/lib/x86/cairo.lib")
-            set(Cairo_DLLS "${Cairo_ROOT}/lib/x86/cairo.dll")
-        endif()
+        set(Cairo_LIBS "${Cairo_ROOT}/lib/x64/cairo.lib")
+        set(Cairo_DLLS "${Cairo_ROOT}/lib/x64/cairo.dll")
     endif()
 
     mark_as_advanced(Cairo_ROOT)
     find_package_handle_standard_args(Cairo DEFAULT_MSG Cairo_INCLUDE_DIRS Cairo_LIBS Cairo_DLLS)
+
 else()
     # On MacOS, should be installed via Macports
     # On Ubuntu, install with: apt-get install libcairo2-dev
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,10 @@
 #include "LSystem2D.hpp"
 #include "TestTurtle.hpp"
 #include "Parser.hpp"
+#include <cairo.h>
+
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 
 int main() {
@@ -19,6 +23,25 @@ int main() {
     Parser p{a};
 
     p.parse_and_draw(result);
+
+    cairo_surface_t* surface = cairo_image_surface_create(CAIRO_FORMAT_RGB24, 400, 400);
+    cairo_t* cr = cairo_create(surface);
+    cairo_set_source_rgb(cr, 1, 1, 1);
+    cairo_paint(cr);
+    cairo_set_source_rgb(cr, 0, 0, 0);
+    for (int i = 0; i <= 10000; i++) {
+        double x = 200 + cos(2 * M_PI * i / 500) * 70 + cos(2 * M_PI * i / 10000) * 110;
+        double y = 200 + sin(2 * M_PI * i / 500) * 70 + sin(2 * M_PI * i / 10000) * 110;
+        if (i == 0)
+            cairo_move_to(cr, x, y);
+        else
+            cairo_line_to(cr, x, y);
+    }
+    cairo_close_path(cr);
+    cairo_stroke(cr);
+    cairo_surface_write_to_png(surface, "spiral.png");
+    cairo_destroy(cr);
+    cairo_surface_destroy(surface);
 
     return 0;
 }
