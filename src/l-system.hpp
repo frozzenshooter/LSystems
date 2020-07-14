916abaa74c702c6a5950532381a8c40e3105c151
Steffen Knoblauch <knoblauch.steffen@web.de>
2020-07-14 17:22:14 +0200

    L-System additions

--- src/l-system.hpp
+++ src/l-system.hpp
@@ -1,8 +1,10 @@
 #ifndef L_SYSTEM_HPP
 #define L_SYSTEM_HPP
 
+#include <algorithm>
 #include <map>
 #include <vector>
+#include <list>
 #include <string>
 #include <memory>
 #include "production-rule.hpp"
@@ -10,27 +12,68 @@
 
 class LSystem {
 
+private:
     class Node {
     public:
-        Node(const std::string& start_axiom) {
+        Node(std::list<std::string>& strings, const std::string& relevant_data)
+        {
+             auto it = std::find(strings.begin(), strings.end(), relevant_data);
 
-        }
+             if (it != strings.end()) {
 
-        std::size_t responsible_index_start_;
-        std::size_t responsible_index_end_;
+                 // save the iterator
+                 relevant_string_it_ = it;
+
+             } else {
+
+                 // new string needs to be added and iterator hold
+                 strings.emplace_back(relevant_data);
+                 relevant_string_it_ = std::find(strings.begin(), strings.end(), relevant_data);
+             }
+             
+        };
+
+        void replace() {
+
+        };
+
+    private:
+
+        std::list<std::string>::iterator relevant_string_it_;
         std::vector<std::shared_ptr<Node>> children_;
+
+
+
     };
 
 public:
 
-    void replace(std::size_t index, const std::string& replacement);
+    LSystem() {
+
+    }
+
+    ~LSystem() {
+
+    }
+
+    void replace(std::size_t index, const std::string& replacement) {
+
+        // falls keine initale node und index = 0
+        //  -> neue startnode erstellen
+
+        // ansonsten die korrekte node durhc rekursiven aufruf suchen und die neuen unterknoten erstellen
+        // update der relevanten indices 
+
+    };
+
+
     void get_char(std::size_t index);
     std::size_t size();
 
 
 private:
     std::shared_ptr<Node> start_node_;
-    std::vector<std::shared_ptr<std::string>> strings_;
+    std::list<std::string> strings_;
 };
 
 #endif
