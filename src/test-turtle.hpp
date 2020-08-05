44dd392212259ae62bfdd285629f87efed4fbf26
Steffen Knoblauch <knoblauch.steffen@web.de>
2020-08-05 10:57:07 +0200

    Test turtle simplified

--- src/test-turtle.hpp
+++ src/test-turtle.hpp
@@ -1,6 +1,7 @@
 #ifndef TEST_TUTRLE_HPP
 #define TEST_TUTRLE_HPP
 
+#include <iostream>
 #include "turtle.hpp"
 
 /*
@@ -12,9 +13,20 @@ public:
     ~TestTurtle() {};
 
     // === drawing functions ========================================
-    void move() override;
-    void draw() override;
-    void turn_right() override;
-    void turn_left() override;
+    void move() override {
+        std::cout << "[Draw-Call]: move" << std::endl;
+    }
+
+    void draw() override {
+        std::cout << "[Draw-Call]: draw" << std::endl;
+    }
+
+    void turn_right() override {
+        std::cout << "[Draw-Call]: turn_right" << std::endl;
+    }
+
+    void turn_left() override {
+        std::cout << "[Draw-Call]: turn_left" << std::endl;
+    }
 };
 #endif
